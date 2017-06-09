3967803f3a0bd624856b08a5a33b2c8f40792de6
Gage Coprivnicar <super15gage@gmail.com>
2017-06-09 08:53:31 -0600

    more bug fixes

--- trade.go
+++ trade.go
@@ -9,7 +9,7 @@ import (
 func buy(currency string, amount float32, price float32, client *bitfinex.Client) int64 {                      //buy function
   var typeOfCoinToSell string = currency + "usd"
 
-  coinToSell = ToUpper(bitfinex.currency + "USD")
+  coinToSell := ToUpper(typeOfCoinToSell)
   data, err := client.Orders.Create(coinToSell, amount, price, bitfinex.OrderTypeExchangeLimit)
 
   if err != nil {
@@ -25,7 +25,7 @@ func sell(currency string, amount float32, price float32, client *bitfinex.Clien
   var typeOfCoinToSell string = currency + "usd"
   var negAmount float32 = amount * -1
 
-  coinToSell = ToUpper(bitfinex.currency + "USD")
+  coinToSell = ToUpper(typeOfCoinToSell)
   negAmount = -1*amount
   data, err := client.Orders.Create(coinToSell, negAmount, price, bitfinex.OrderTypeExchangeLimit)
 
@@ -37,11 +37,11 @@ func sell(currency string, amount float32, price float32, client *bitfinex.Clien
 
 }
 
-func sellMarketOrder(currency string, amount float32, price float32 client *bitfinex.Client) {
+func sellMarketOrder(currency string, amount float32, price float32, client *bitfinex.Client) {
   var typeOfCoinToSell string = currency + "usd"
   var negAmount float32 = amount * -1
 
-  coinToSell = ToUpper(bitfinex.currency + "USD")
+  coinToSell = ToUpper(typeOfCoinToSell)
   negAmount = -1*amount
   data, err := client.Orders.Create(coinToSell, negAmount, price, bitfinex.OrderTypeExchangeMarket)
 
